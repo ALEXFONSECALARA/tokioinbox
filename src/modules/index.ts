1994e1b752cd2cba653eaea813d1cf7609452197
TokioinBox <claudiano.222@gmail.com>
2026-09-25 00:06:14 -0300

    Delete src directory

--- src/modules/index.ts
+++
@@ -1,7 +0,0 @@
-export { SalaoModule } from './salao/SalaoModule';
-export { ClienteModule } from './cliente/ClienteModule';
-export { BalcaoModule } from './balcao/BalcaoModule';
-export { DeliveryModule } from './delivery/DeliveryModule';
-export { CaixaModule } from './caixa/CaixaModule';
-export { ProducaoModule } from './producao/ProducaoModule';
-export { AdminModule } from './admin/AdminModule';
