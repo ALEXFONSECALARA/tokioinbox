86bd0035880d726839ed54b67de959084ac12988
TokioinBox <claudiano.222@gmail.com>
2026-09-25 00:28:30 -0300

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { SalaoModule } from './salao/SalaoModule';
+export { ClienteModule } from './cliente/ClienteModule';
+export { BalcaoModule } from './balcao/BalcaoModule';
+export { DeliveryModule } from './delivery/DeliveryModule';
+export { CaixaModule } from './caixa/CaixaModule';
+export { ProducaoModule } from './producao/ProducaoModule';
+export { AdminModule } from './admin/AdminModule';
